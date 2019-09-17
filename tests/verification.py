7a652076c43646ff62843cd879d08de944e92e27
Niels van Huijstee <niels@fuga.cloud>
2019-09-17 21:36:30 +0200

    Test "real" signature verification.
    closes #2

--- tests/verification.py
+++ tests/verification.py
@@ -1,9 +1,12 @@
+import os
+import math
+import time
 import itertools
-
-import pytest
 
 from starlette.status import HTTP_403_FORBIDDEN, HTTP_422_UNPROCESSABLE_ENTITY
 from starlette.testclient import TestClient
+
+import pytest
 
 paths = ("/events", "/actions", "/commands")
 incomplete_headers = (
@@ -27,3 +30,20 @@ def post_commands_should_verify_headers(path: str, client: TestClient):
 def post_commands_should_require_headers(path: str, headers: dict, client: TestClient):
     response = client.post(url=path, data={}, headers=headers)
     assert HTTP_422_UNPROCESSABLE_ENTITY == response.status_code
+
+
+def post_commands_should_be_verified(client: TestClient):
+    os.environ["SLACK_SIGNING_SECRET"] = "TEST_SECRET"
+    timestamp_jan_1_2019_noon = "1546340400"
+    signature = "v0=66c758f7c180af608f5984e07c562ad8033c2ccce8b21771655fa7dd8d480ebe"
+    valid_headers = {
+        "X-Slack-Request-Timestamp": timestamp_jan_1_2019_noon,
+        "X-Slack-Signature": signature,
+    }
+    challenge = {
+        "token": "SLACK_TOKEN",
+        "challenge": "A REAL CHALLENGE",
+        "type": "url_verification",
+    }
+    response = client.post("/events", json=challenge, headers=valid_headers)
+    assert 200 == response.status_code
