167ff872b479dadfdf823b1b7dd18ba8adfeb23b
Niels van Huijstee <niels@huijs.net>
2020-04-30 22:09:09 +0200

    Add test for optional authed_users.

--- tests/events.py
+++ tests/events.py
@@ -1,3 +1,4 @@
+import copy
 import pytest
 
 from starlette.status import HTTP_200_OK
@@ -50,3 +51,35 @@ def post_events_should_emit_events_event_with_payload(
 
     assert HTTP_200_OK == response.status_code
     inspection.assert_called_once_with(payload=slack_envelope)
+
+
+@pytest.mark.usefixtures("pass_header_verification")
+def post_events_should_accept_envelope_without_authed_users(
+        mocker, client: TestClient, test_headers
+):
+    slack_envelope = {
+        "token": "TOKEN",
+        "team_id": "TEAM_ID",
+        "api_app_id": "API_APP_ID",
+        "event": {"type": "foo"},
+        "type": "TYPE",
+        "event_id": "EVENT_ID",
+        "event_time": 1,
+    }
+
+    # Optional field will be filled with None
+    expected_payload = copy.copy(slack_envelope)
+    expected_payload["authed_users"] = None
+
+    @events.on("foo")
+    def on_foo(payload):
+        import json
+        print(json.dumps(payload))
+        inspection(payload=payload)
+
+    inspection = mocker.Mock()
+
+    response = client.post(url="/events", json=slack_envelope, headers=test_headers)
+
+    assert HTTP_200_OK == response.status_code
+    inspection.assert_called_once_with(payload=expected_payload)
